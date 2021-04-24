de46bbc711bcc20eb302b39568dad6b79c76f5a9
alexlaggner <allaggner@edu.aau.at>
2021-04-24 14:46:21 +0200

    Update index.js
    check if heroku updates properly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import net from 'net';
+/*import net from 'net';
 
 const PORT = process.env.PORT || 5001;
 const host = process.env.HOST || "0.0.0.0";
@@ -8,3 +8,12 @@ server.listen(PORT, host, function () {
    console.log(`abalone server running \nPORT: ${PORT} \Host: ${host}`);
 });
 
+*/
+
+import express from 'express';
+
+const app = express();
+app.listen((process.env.PORT || 5001), (req, res) => {
+res.send("Geht")
+
+});
