18df9484fe0fccfdb0a25e15a7b6451818cc64cd
alexlaggner <allaggner@edu.aau.at>
2021-04-24 14:14:14 +0200

    Update index.js
    minor fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ let app = express()
 const io = require('socket.io')(server);
 
 const PORT  = process.env.PORT || 5001
-app.listen(PORT,()=> console.info(`Server has started on ${PORT}`))
+app.listen(PORT,(req,res)=> res.send(`Server has started on ${PORT}`))
 
 app.get("/", (req, res) => {
     res.send("welcome to the abalone mobile app api");
