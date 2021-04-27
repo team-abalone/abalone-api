66ec10f3842290af4b42fea7ae4cadf20b528dc8
alexlaggner <allaggner@edu.aau.at>
2021-04-27 10:11:59 +0200

    socket aktionen werden unterschieden, controls ordner wurde erstellt
    Funktionen kommen der übersicht halber in Controls/controls.js, diese werden per require importiert

--- index.js
+++ index.js
@@ -6,20 +6,39 @@ const host = process.env.HOST || "127.0.0.1";
 let server = net.createServer();
 
 server.listen(PORT, host, function () {
-    console.log(`abalone server running \nPORT: ${PORT} \Host: ${host}\nServer Adress: ${JSON.stringify(server.adress)}`);
+    console.log(`Abalone server running \nPORT: ${PORT} \Host: ${host}\n`);
 });
 
 server.on("connection", function (socket) {
+    //getting sockets adress for connecting non-host-players later on
     var remoteAdress = socket.remoteAdress + socket.remotePort;
     console.log("Connected");
 
+    //when socket is build:
     socket.on("data", function (d) {
         /*
          * Functions that handle input go here
          * Chat : gets message, returns array [playername][msg] -> display in app
-         *
+         * Join: accepts parameter
          * */
-       
+
+        //received data should start with '0', '1' or '2' to indicate which functions to use
+        //0 : join another socket
+        //1 : write lobby chat
+        //2 : game logic
+        // further types could occur depending on final game (cheat function, etc)
+
+        let type = (d+'').split(" ")[0];
+
+        if (type == 0) {
+            console.log("case1");
+        }
+        else if (type == 1) {
+            console.log("case2");
+        }
+        else if (type == 2) {
+            console.log("case3");
+        }
     });
 
     socket.on("error", function (error) {
@@ -31,5 +50,7 @@ server.on("connection", function (socket) {
     });
 
 });
+
+
 
 
