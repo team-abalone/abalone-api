74e35983bc5b116ebbfe4c64074a6eb1bdc7e70d
Lukas Kohlmaier <luko0610@gmail.com>
2021-05-29 12:37:03 +0200

    ADD - Added UserNameMap to responses / requests.

--- src/index.js
+++ src/index.js
@@ -109,6 +109,7 @@ server.on("connection", function (socket) {
             players: room?.players,
             createdBy: room?.createdBy,
             numberOfPlayers: room?.numberOfPlayers,
+            roomMap: room?.roomMap,
           })
         );
 
@@ -119,6 +120,7 @@ server.on("connection", function (socket) {
           players: room?.players,
           createdBy: room?.createdBy,
           numberOfPlayers: room?.numberOfPlayers,
+          roomMap: room?.roomMap,
         });
       } else if (commandCode === InCommandCodes.CloseRoom) {
         let room = roomControls.closeRoom(userId, roomKey);
