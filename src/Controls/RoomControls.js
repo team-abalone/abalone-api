765405256aae59de702441ca327ba59507c5ed56
alexlaggner <allaggner@edu.aau.at>
2021-06-10 16:48:37 +0200

    changed 'type' to 'characters'

--- src/Controls/RoomControls.js
+++ src/Controls/RoomControls.js
@@ -40,7 +40,11 @@ class RoomControls {
     }
 
     let room = {
-      roomKey: cryptoRandomString({ length: 5, type: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9" }),
+      roomKey: cryptoRandomString({
+        length: 5,
+        characters:
+          "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9",
+      }),
       createdBy: userId,
       players: [userId],
       numberOfPlayers: numberOfPlayers,
