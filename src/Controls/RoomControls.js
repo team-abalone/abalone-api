6afc85d32716dcbf84ab8d56014b711a2b596da1
alexlaggner <allaggner@edu.aau.at>
2021-06-10 16:52:29 +0200

    remove ','

--- src/Controls/RoomControls.js
+++ src/Controls/RoomControls.js
@@ -43,7 +43,7 @@ class RoomControls {
       roomKey: cryptoRandomString({
         length: 5,
         characters:
-          "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9",
+          "abcdefghijklmnopqrstuvwxyz0123456789",
       }),
       createdBy: userId,
       players: [userId],
